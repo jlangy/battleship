d45be53387fc1525426afb11de7c756095deef2b
Jonathan Langlois <jonathan-langlois@Live.ca>
2020-01-15 03:49:44 +0000

    begin shootSquare function

--- js/index.js
+++ js/index.js
@@ -5,14 +5,14 @@ const isPlacable = require('./placingPhase').isPlacable;
 const hasSquare = require('./helpers').hasSquare;
 const equalSquares = require('./helpers').equalSquares;
 const findShipsOccupiedSquares = require('./placingPhase').findShipsOccupiedSquares;
-
-
+const getPlayerSquares = require('./helpers').getPlayerSquares;
 
 state = {
-  p1Turn: true,
-  p2Turn: false,
+  p1Turn: false,
+  p2Turn: true,
   selectedShip: 'destroyer',
   shipOrientation: 0,
+  board: generateBoard(),
   p1Board: {
     hitSquares: [],
     missSquares: [],
@@ -25,9 +25,17 @@ state = {
   }
 }
 
-const board = generateBoard();
+const shootSquare = function(square){
+  const playerBoardKey = state.p1Turn ? "p1Board": "p2Board";
+  if(hasSquare(state[playerBoardKey].hitSquares, square) || hasSquare(state[playerBoardKey].missSquares, square)){
+    return;
+    //Can't reshoot, send some msg here
+  }
+  const opponentBoardKey = state.p1Turn ? "p2Board" : "p1Board";
+  const opponentShipSquares = getPlayerSquares(state[opponentBoardKey].ships);
+  //if matches a ship, add to player hits
+    //if sunk ship remove ship
+  //if miss, add to player misses
+}
 
-printBoard(state.p1Board.ships);
-place([7,7], board);
-printBoard(state.p1Board.ships);
-
+shootSquare();
