8e02abd73469d88f168f1d285937043aa47dde6f
Jonathan Langlois <jonathan-langlois@Live.ca>
2020-01-25 22:51:35 +0000

    update the players board to show previous shots

--- public/js/board.js
+++ public/js/board.js
@@ -26,7 +26,19 @@ const renderBoard = function(){
   for(let i = 0; i < 10; i++){
     let row = i;
     for(let j = 0; j < 10; j++){
-      $('#board').append(`<div class='square' id='${i}${j}'></div>`);
+      if(state.playPhase === "gamePlay"){
+        const currentSquare =  getSquareFromId(`${i}${j}`);
+        const playerMisses = state[state.currentBoard].missSquares;
+        const playerHits = state[state.currentBoard].hitSquares;
+        if(hasSquare(playerHits, currentSquare)){
+          $('#board').append(`<div class='square hitSquare' id='${i}${j}'></div>`);
+        }
+        else if(hasSquare(playerMisses, currentSquare)){
+          $('#board').append(`<div class='square missSquare' id='${i}${j}'></div>`);
+        } else{
+          $('#board').append(`<div class='square' id='${i}${j}'></div>`);
+        }
+      }
     }
   }
 }
@@ -35,26 +47,27 @@ const renderDisplayBoard = function(){
   for(let i = 0; i < 10; i++){
     let row = i;
     for(let j = 0; j < 10; j++){
-      if(state.playPhase === "gamePlay"){
-        const currentSquare =  getSquareFromId(`${i}${j}`);
-        const playerShipSquares = getPlayerSquares(state[state.currentBoard].ships);
-        const opponentsMisses = state[state.opponentBoard].missSquares;
-        const opponentsHits = state[state.opponentBoard].hitSquares;
-        //check if it is in opponent hit or miss squares FIRST, since then the ship colour doesnt matter
-        if(hasSquare(opponentsHits, currentSquare)){
-          $('#displayBoard').append(`<div class='square hitSquare' id='d${i}${j}'></div>`);
-        }
-        else if(hasSquare(opponentsMisses, currentSquare)){
-          $('#displayBoard').append(`<div class='square missSquare' id='d${i}${j}'></div>`);
-        }
-        else if (hasSquare(playerShipSquares, currentSquare)){
-          $('#displayBoard').append(`<div class='square hasShip' id='d${i}${j}'></div>`);
-        } else {
-        $('#displayBoard').append(`<div class='square' id='d${i}${j}'></div>`);
-        }
-      }
+      drawDisplaySquare(i,j);
     }
   }
 }
 
+const drawDisplaySquare = (i,j) => {
+  const currentSquare =  getSquareFromId(`${i}${j}`);
+  const playerShipSquares = getPlayerSquares(state[state.currentBoard].ships);
+  const opponentsMisses = state[state.opponentBoard].missSquares;
+  const opponentsHits = state[state.opponentBoard].hitSquares;
+  //check if it is a shipsquare, hitsquare, or misssquare to add correct classes
+  if(hasSquare(opponentsHits, currentSquare)){
+    $('#displayBoard').append(`<div class='square hitSquare' id='d${i}${j}'></div>`);
+  }
+  else if(hasSquare(opponentsMisses, currentSquare)){
+    $('#displayBoard').append(`<div class='square missSquare' id='d${i}${j}'></div>`);
+  }
+  else if (hasSquare(playerShipSquares, currentSquare)){
+    $('#displayBoard').append(`<div class='square hasShip' id='d${i}${j}'></div>`);
+  } else {
+  $('#displayBoard').append(`<div class='square' id='d${i}${j}'></div>`);
+  }
+}
 
