fd4cb90c77acf6c78a00a49fabcd5d96234ffe85
Jonathan Langlois <jonathan-langlois@Live.ca>
2020-01-25 02:11:44 +0000

    refactor setBoardPlacementListeners

--- public/js/board.js
+++ public/js/board.js
@@ -22,6 +22,7 @@ const printBoard = function(player){
 }
 
 const renderBoard = function(){
+  $('#board').empty();
   for(let i = 0; i < 10; i++){
     let row = i;
     for(let j = 0; j < 10; j++){
--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,6 @@
 state = {
   playerTurn: 0,
+  currentBoard: "p1Board",
   selectedShip: 0,
   //for orientation 0 = north 1 = east 2 south 3 west
   shipOrientation: 1,
@@ -18,20 +19,30 @@ state = {
 
 const setBoardPlacementListeners = (event) => {
   const clickedSquare = getSquareFromId(event.target.id);
-  const playerBoard = state.playerTurn == 0 ? "p1Board" : "p2Board";
-  console.log(state.playerTurn);
-  const placed = place(clickedSquare, state[playerBoard]);
+  const placed = place(clickedSquare, state[state.currentBoard]);
   if(placed){
-    for(let square of findShipsOccupiedSquares(clickedSquare)){
-      let squareId = getIdFromSquare(square);
-      $(squareId).addClass("hasShip")
-    }
-    if(state.selectedShip < 4){
-      state.selectedShip += 1;
-    } else {
-      console.log(state[playerBoard].availableSquares);
-      state.playerTurn = (state.playerTurn == 0 ? 1 : 0);
+    renderShipToBoard(clickedSquare);
+  }
+}
+
+const renderShipToBoard = (clickedSquare) => {
+  for(let square of findShipsOccupiedSquares(clickedSquare)){
+    let squareId = getIdFromSquare(square);
+    $(squareId).addClass("hasShip")
+  }
+  incrementPlacePhase();
+}
+
+const incrementPlacePhase = () => {
+  if(state.selectedShip < 4){
+    state.selectedShip += 1;
+  } else {
+    if(state.playerTurn === 0){
+      state.playerTurn = 1;
+      state.currentBoard = "p2Board";
       state.selectedShip = 0;
+    } else {
+      beginPlayPhase();
     }
   }
 }
